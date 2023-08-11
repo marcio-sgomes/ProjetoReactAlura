9a482f5aad2371579cec39ddad9c4ffb94e33ce2
marcio-sgomes <marcio.sant.gomes@gmail.com>
2023-08-11 16:23:54 -0300

    aaaaaaaa

---
+++ src/componentes/BackGround/index.js
@@ -0,0 +1,14 @@
+// Background.js
+
+import React from 'react';
+import './Background.css';
+
+const Background = () => {
+  return (
+    <div className="background-container">
+    
+    </div>
+  );
+};
+
+export default Background;
